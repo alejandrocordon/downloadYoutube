85948ec0b7db3a68fd0d6df4a428995e749b4ea3
Alejandro Cordon Urena <cordon.alejandro@gmail.com>
2021-10-01 22:57:19 +0200

    download video from youtube py

---
+++ downloadYoutube.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+
+link = input("Introduce url de youtube:")
+yt = YouTube(link)
+
+stream = yt.streams.get_highest_resolution()
+
+print("Iniciando descarga...")
+stream.download()
+print("Descarga finalizada.")
