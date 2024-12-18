d490fc06e5ae1ba06e377b6d2c4416ae0f4a5b5e
Alejandro <cordon.alejandro@gmail.com>
2024-12-18 22:16:58 +0100

    updated to new version

--- downloadYoutube.py
+++ downloadYoutube.py
@@ -1,11 +1,25 @@
-# remember :  pip install pytube
-from pytube import YouTube
+import yt_dlp
 
-link = input("Introduce url de youtube:")
-yt = YouTube(link)
+def descargar_videos(urls, destino='.'):
+    ydl_opts = {
+        'outtmpl': f'{destino}/%(playlist_index)s - %(title)s.%(ext)s',  # Ruta y nombre del archivo de salida
+        'format': 'bestvideo+bestaudio/best',       # Selecciona la mejor calidad de video y audio
+        'noplaylist': True,                         # Evita descargar listas de reproducción enteras si se proporciona una URL de lista
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(urls)
+        print("Descarga completada.")
+    except Exception as e:
+        print(f"Ocurrió un error al descargar los videos: {e}")
 
-stream = yt.streams.get_highest_resolution()
+if __name__ == "__main__":
+    urls = [
+        "https://youtu.be/XXXXX",
+        "https://youtu.be/XXX"
 
-print("Iniciando descarga...")
-stream.download()
-print("Descarga finalizada.")
+
+
+        # Agrega más URLs según sea necesario
+    ]
+    descargar_videos(urls, destino='videos')
